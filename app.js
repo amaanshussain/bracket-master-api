476399ee84a895bb72b27a56c200fe5c4d4dc76d
Amaan <55152090+amaanshussain@users.noreply.github.com>
2025-03-02 00:41:44 -0600

    Feature/create routes (#1)
    * add routes
    
    * integrate env file

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+
+app.use('/api/teams', require('./routes/teams'));
+
+app.use('/api/brackets', require('./routes/brackets'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+
+
+
+module.exports = router;
